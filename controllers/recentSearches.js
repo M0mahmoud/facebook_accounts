35e43492b69f44d319c4d1ec7ae4587a91774ea2
M0mahmoud <houdmohamed85@gmail.com>
2024-02-23 17:07:59 +0200

    Fixing RecentSearch

--- controllers/recentSearches.js
+++ controllers/recentSearches.js
@@ -1,18 +1,22 @@
 const UserDB = require("../db/users");
 const RecentSearch = require("../models/RecentSearch.Model");
+const User = require("../models/User.Model");
 
 const recentSearches = async (req, res) => {
   const { code } = req.body;
   try {
     await UserDB();
-    const user = await RecentSearch.findOne({ code });
+    const user = await User.findOne({ code });
     if (!user) {
-      return res
-        .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" })
-        .status(404);
+      return res.json({ msg: "ادخل كود صحيح" }).status(404);
     }
+    let recentSearch = await RecentSearch.findOne({ code });
+    if (!recentSearch) {
+      recentSearch = new RecentSearch({ code, id: user.id });
+    }
+    await recentSearch.save();
 
-    res.status(200).json({ user });
+    res.status(200).json({ user: recentSearch });
   } catch (error) {
     return res
       .json({ msg: "خطأ بالإتصال بالإنترنت", error: error.message })
