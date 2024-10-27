b74ee9ad368502fbada7c4e4c48aabb18d481fef
M0mahmoud <houdmohamed85@gmail.com>
2024-10-27 08:10:20 +0300

    New API

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ADMIN_05, ADMIN_USF, collectionName } from "../config.js";
 import connectToDatabases from "../db/database.js";
 import UserDB from "../db/users.js";
@@ -41,32 +42,60 @@ export const withIdController = async (req, res) => {
     // Save Recent Search For All.
     await recent.save();
 
-    const databases = await connectToDatabases();
+    // const databases = await connectToDatabases();
     let accountFound = false;
     let accounts = [];
 
-    for (const db of databases) {
-      const collection = db.collection(collectionName);
-      const account = await collection.findOne({ id: facebook_id });
-      console.log("account:", account);
-      if (account) {
-        accountFound = true;
-        accounts.push(account);
-        break;
-      }
-    }
+    // for (const db of databases) {
+    //   const collection = db.collection(collectionName);
+    //   const account = await collection.findOne({ id: facebook_id });
+    //   console.log("account:", account);
+    //   if (account) {
+    //     accountFound = true;
+    //     accounts.push(account);
+    //     break;
+    //   }
+    // }
 
-    if (accountFound) {
-      if (user.id !== ADMIN_05 && user.id !== ADMIN_USF) {
-        user.points = user.points - 1;
-      }
-      await user.save();
-      return res.json({ data: accounts, points: user.points }).status(200);
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
-    }
+    axios
+      .get(`http://174.129.144.145/search?term=${facebook_id}`)
+      .then((response) => {
+        const result = response.data;
+        console.log("🚀 ~ .then ~ result:", result);
+        if (!result) {
+          accountFound = false;
+          return res
+            .status(404)
+            .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
+        } else {
+          accountFound = true;
+          accounts.push({
+            firstname: result.data.first_name,
+            lastname: result.data.last_name,
+            phone: result.data.phone,
+            url: result.data.url,
+            id: result.data.id,
+          });
+        }
+
+        if (accountFound) {
+          if (user.id !== ADMIN_05 && user.id !== ADMIN_USF) {
+            user.points = user.points - 1;
+          }
+          user.save();
+          return res.json({ data: accounts, points: user.points }).status(200);
+        } else {
+          return res
+            .status(404)
+            .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return res
+          .status(404)
+          .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
+      });
   } catch (err) {
     console.error(err);
     return res
@@ -81,7 +110,7 @@ export const searchWithPhone = async (req, res) => {
     if (!phone) {
       return res.json({ msg: "Value Required!" }).status(400);
     }
-    const phone_number = `+2${phone}`;
+    // const phone_number = `+2${phone}`;
     await UserDB();
     const user = await User.findOne({ id });
     if (!user) {
@@ -108,36 +137,63 @@ export const searchWithPhone = async (req, res) => {
     }
 
     if (user.id !== ADMIN_05) {
-      recent.key.push(phone_number);
+      recent.key.push(phone);
     }
     // Save Recent Search For all.
     await recent.save();
 
-    const databases = await connectToDatabases();
+    // const databases = await connectToDatabases();
     let accountFound = false;
     let accounts = [];
 
-    for (const db of databases) {
-      const collection = db.collection(collectionName);
-      const account = await collection.findOne({ phone: phone_number });
-      if (account) {
-        accountFound = true;
-        accounts.push(account);
-        break;
-      }
-    }
+    // for (const db of databases) {
+    // const collection = db.collection(collectionName);
+    // const account = await collection.findOne({ phone: phone_number });
+    // if (account) {
+    // accountFound = true;
+    // accounts.push(account);
+    // break;
+    // }
+    // }
 
-    if (accountFound) {
-      if (user.id !== ADMIN_05 && user.id !== ADMIN_USF) {
-        user.points = user.points - 1;
-      }
-      await user.save();
-      return res.json({ data: accounts, points: user.points }).status(200);
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
-    }
+    axios
+      .get(`http://174.129.144.145/search?term=${phone}`)
+      .then(async (response) => {
+        const result = response.data;
+        if (!result) {
+          accountFound = false;
+          return res
+            .status(404)
+            .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
+        } else {
+          accountFound = true;
+          accounts.push({
+            firstname: result.data.first_name,
+            lastname: result.data.last_name,
+            phone: result.data.phone,
+            id: result.data.id,
+            url: result.data.url,
+          });
+        }
+
+        if (accountFound) {
+          if (user.id !== ADMIN_05 && user.id !== ADMIN_USF) {
+            user.points = user.points - 1;
+          }
+          await user.save();
+          return res.json({ data: accounts, points: user.points }).status(200);
+        } else {
+          return res
+            .status(404)
+            .json({ msg: "هذا الحساب غير متوفر في البيانات المسجلة لدينا" });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return res
+          .json({ msg: "خطأ بالإتصال بالإنترنت", error: err.message })
+          .status(404);
+      });
   } catch (err) {
     console.error(err);
     return res
